2e432238c66e43d09d1b9e7c53d0de0f53ace520
Nestearovich <dante_vit@mail.ru>
2022-11-23 22:07:21 +0300

    19 дз

--- app/src/main/java/com/example/androidprhome/MainActivity.kt
+++ app/src/main/java/com/example/androidprhome/MainActivity.kt
@@ -4,15 +4,28 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.RadioButton
+import com.google.android.material.textfield.TextInputEditText
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnGoToActivity2 = findViewById<Button>(R.id.btnGoToActivity2)
-        btnGoToActivity2.setOnClickListener {
-            startActivity(Intent(this, MainActivity2::class.java))
+        val rbtn = findViewById<RadioButton>(R.id.rbtn1)
+        val rbtn2 = findViewById<RadioButton>(R.id.rbtn2)
+
+        rbtn.setOnClickListener {
+            if (rbtn.isChecked) {
+                rbtn2.isChecked = false
+            }
+        }
+        rbtn2.setOnClickListener {
+            if (rbtn2.isChecked) {
+                rbtn.isChecked = false
+            }
         }
     }
 }
